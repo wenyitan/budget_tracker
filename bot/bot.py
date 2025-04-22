61e82a497ce5c50ad30cb597656636b41c2e8be0
wenyitan <tanwenyi1511@gmail.com>
2025-04-22 23:52:31 +0800

    complete logic for flow, also add category_id to transaction

--- bot/bot.py
+++ bot/bot.py
@@ -39,11 +39,11 @@ def register_amount_prompt_person(message):
         markup.add("Wens")
         markup.add("Tians")
         sent_message = bot.send_message(chat.id, text=text, reply_markup=markup)
-        bot.register_next_step_handler(message=sent_message, callback=register_person_prompt_date, transaction=transaction)
+        bot.register_next_step_handler(message=sent_message, callback=register_person_prompt_shared, transaction=transaction)
     except ValueError:
         pass
 
-def register_person_prompt_date(message, **kwargs):
+def register_person_prompt_shared(message, **kwargs):
     chat = message.chat
     person = message.text
     if person not in ["Wens", "Tians"]:
@@ -58,7 +58,7 @@ def register_person_prompt_date(message, **kwargs):
         markup.add("No")
         text = f"OK. Is it a shared transaction"
         sent_message = bot.send_message(chat.id, text=text, reply_markup=markup)
-        bot.register_next_step_handler(message=sent_message, callback=handle_shared_yes_no, transaction=transaction)
+        bot.register_next_step_handler(message=sent_message, callback=handle_shared_yes_no_prompt_category, transaction=transaction)
 
 def handle_today_yes_no(message, **kwargs):
     chat = message.chat
@@ -68,11 +68,10 @@ def handle_today_yes_no(message, **kwargs):
     if answer == "Yes":
         transaction.date = date
         markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-        markup.add("Yes")
-        markup.add("No")
-        text = "OK. Do you have any comments?"
+        markup.add("No comments!")
+        text = "OK. Please leave your comments about this transaction. Press 'No comments!' if you don't have any."
         sent_message = bot.send_message(chat.id, text=text, reply_markup=markup)
-        bot.register_next_step_handler(message=sent_message, callback=handle_shared_yes_no, transaction=transaction)
+        bot.register_next_step_handler(message=sent_message, callback=handle_comments_prompt_consolidate, transaction=transaction)
     elif answer == "No":
         markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
         month = date.split("-")[1]
@@ -104,9 +103,9 @@ def handle_month_prompt_year(message, **kwargs):
     transaction = kwargs['transaction']
     text = "Which year was it? (Please type and provide a 4-digit year)"
     sent_message = bot.send_message(chat.id, text=text)
-    bot.register_next_step_handler(message=sent_message, callback=handle_year_prompt_shared, transaction=transaction, day=day, month=month)
+    bot.register_next_step_handler(message=sent_message, callback=handle_year_prompt_comments, transaction=transaction, day=day, month=month)
 
-def handle_year_prompt_shared(message, **kwargs):
+def handle_year_prompt_comments(message, **kwargs):
     chat = message.chat
     year = message.text
     month = kwargs['month']
@@ -115,18 +114,57 @@ def handle_year_prompt_shared(message, **kwargs):
     transaction.date = f"{day}-{month}-{year}"
     print(transaction.date)
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-    markup.add("Yes")
-    markup.add("No")
-    text = "OK. Do you have any comments?"
+    markup.add("No comments!")
+    text = "OK. Please leave your comments about this transaction. Press 'No comments!' if you don't have any."
     sent_message = bot.send_message(chat.id, text=text, reply_markup=markup)
-    bot.register_next_step_handler(message=sent_message, callback=handle_shared_yes_no, transaction=transaction)
+    bot.register_next_step_handler(message=sent_message, callback=handle_comments_prompt_consolidate, transaction=transaction)
 
+def handle_comments_prompt_consolidate(message, **kwargs):
+    answer = message.text
+    transaction = kwargs['transaction']
+    if answer != "No comments!":
+        transaction.description = answer
+    text = f"Ok. Please check your transaction: {transaction.__dict__}"
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    markup.add("Save!", "Abort!", row_width=2)
+    sent_message = bot.send_message(message.chat.id, text=text)
+    bot.register_next_step_handler(message=sent_message, callback=handle_save_or_abort, transaction=transaction)
 
-def handle_shared_yes_no(message, **kwargs):
-    print(kwargs['transaction'].__dict__)
+def handle_save_or_abort(message, **kwargs):
+    transaction = kwargs['transaction']
+    choice = message.text
+    if choice == "Save!":
+        text = "Ok saving transaction..."
+    else:
+        text ="Ok aborting..."
+    markup = types.ReplyKeyboardRemove()
+    bot.send_message(message.chat.id, text=text, reply_markup=markup)
+
+def handle_shared_yes_no_prompt_category(message, **kwargs):
     answer = message.text.lower()
     transaction = kwargs['transaction']
     transaction.shared = answer == "yes"
+    text = "Sure! Which category does this expense belong to?"
+    category_placeholder = ["Food", "Baby", "Groceries", "Add new category"]
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    markup.add(*category_placeholder, row_width=2)
+    sent_message = bot.send_message(message.chat.id, text=text, reply_markup=markup)
+    bot.register_next_step_handler(message=sent_message, callback=handle_category_response_prompt_date, transaction=transaction, categories=category_placeholder)
+
+def handle_category_response_prompt_date(message, **kwargs):
+    answer = message.text
+    transaction = kwargs['transaction']
+    categories = kwargs['categories']
+    if answer == "Add new category":
+        pass
+    else:
+        transaction.category_id = answer
+        print(transaction.__dict__)
+        text = "Roger that! Was it spent today?"
+        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+        markup.add("Yes", "No", row_width=2)
+        sent_message = bot.send_message(message.chat.id, text=text, reply_markup=markup)
+        bot.register_next_step_handler(message=sent_message, callback=handle_today_yes_no, transaction=transaction)
 
 
 if __name__ == "__main__":
--- bot/transaction.py
+++ bot/transaction.py
@@ -1,8 +1,9 @@
 class Transaction():
-    def __init__(self, amount, person="", date="", description="", shared=None, id=None):
+    def __init__(self, amount, person="", date="", description="", shared=None, id=None, category_id=None):
         self.id = id
         self.amount = amount
         self.person = person
         self.date = date
         self.description = description
         self.shared = shared
+        self.category_id = category_id
