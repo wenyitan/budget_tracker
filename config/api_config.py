49386786f6e2f257bcf074f583a9cc62ce93b88a
wenyitan <tanwenyi1511@gmail.com>
2025-05-18 00:38:04 +0800

    rename env variables and update workflow

--- config/api_config.py
+++ config/api_config.py
@@ -3,7 +3,7 @@ from config.env_config import test_env, env
 
 def load_jwt_key():
     if env == "test" and test_env == "github-actions":
-        return os.getenv("jwt_secret_key")
+        return os.getenv("JWT_SECRET_KEY")
     else:
         from config.secrets import JWT_SECRET_KEY
         return JWT_SECRET_KEY
--- config/bot_config.py
+++ config/bot_config.py
@@ -6,7 +6,7 @@ DATE_FORMAT = "%d-%b-%Y"
 
 def load_allowed_users():
     if env == "test" and test_env == "github-actions":
-        return json.loads(os.getenv("allowed_users"))
+        return json.loads(os.getenv("ALLOWED_USERS"))
     else:
         from config.secrets import ALLOWED_USERS
         return ALLOWED_USERS
