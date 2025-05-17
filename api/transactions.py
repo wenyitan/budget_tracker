423a490ff5b2fe5d8060fd13501e6130621ab90f
wenyitan <tanwenyi1511@gmail.com>
2025-05-18 01:31:35 +0800

    add implementation for add_transaction endpoint

--- api/transactions.py
+++ api/transactions.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, abort
+from flask import Blueprint, abort, request
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
 def transactions_bp(db):
@@ -18,5 +18,19 @@ def transactions_bp(db):
             return transaction
         else:
             abort(404)
+
+    @transactions_bp.post("/")
+    @jwt_required()
+    def add_transaction():
+        transaction = request.json
+        amount = transaction["amount"]
+        person = transaction["person"]
+        date = transaction["date"]
+        description = transaction["description"]
+        category_id = transaction["category_id"]
+        query = "insert into transactions (amount, person, date, description, category_id) values (?,?,?,?,?)"
+        db.execute(query, (amount, person, date, description, category_id))
+        last_row_id = db.cursor.lastrowid
+        return {"message": "success", "status_code": 201, "transaction": get_transaction_by_id(last_row_id)}, 201
     
     return transactions_bp
