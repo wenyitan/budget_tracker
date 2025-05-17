d5f551f4afcc4dba239f88425a9e5568f57522e6
wenyitan <tanwenyi1511@gmail.com>
2025-05-18 00:44:15 +0800

    add print

--- config/bot_config.py
+++ config/bot_config.py
@@ -5,6 +5,7 @@ from config.env_config import test_env, env
 DATE_FORMAT = "%d-%b-%Y"
 
 def load_allowed_users():
+    print(env, test_env)
     if env == "test" and test_env == "github-actions":
         return json.loads(os.getenv("ALLOWED_USERS"))
     else:
