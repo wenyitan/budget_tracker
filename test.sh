f35137bf067d82ecc42bc93199eec06e0cb69d8c
wenyitan <tanwenyi1511@gmail.com>
2025-05-29 01:28:54 +0800

    update test.sh with network logic

--- test.sh
+++ test.sh
@@ -7,9 +7,9 @@ docker run --rm \
     -e DATE_FORMAT=%d-%b-%Y \
     -e PYTHONUNBUFFERED=1 \
     -e ENV=test \
+    $(if [ -n "$TEST_ENV" ]; then echo ""; else echo "--network wen-network"; fi) \
     ${TEST_ENV:+-e TEST_ENV="$TEST_ENV"} \
     ${JWT_SECRET_KEY:+-e JWT_SECRET_KEY="$JWT_SECRET_KEY"} \
     ${ALLOWED_USERS:+-e ALLOWED_USERS="$ALLOWED_USERS"} \
-    --network wen-network \
     python:3.11 \
     bash -c "pip install --no-cache-dir -r /app/tests/requirements_test.txt && pytest -vvv --capture=tee-sys"
